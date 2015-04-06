0c6c5d3e945c9a8f1c24f6a2aeac7c72c13fad2c
Christian Jánský <chrisjansky@gmail.com>
2015-04-07 00:20:54 +0200

    JS Updated lg-royal for touch events

--- app/assets/js/lg-royal.js
+++ app/assets/js/lg-royal.js
@@ -4,11 +4,11 @@ var
 
 var
   sliderLock = true,
-  sliderWatch = isDesktop(),
+  desktopOnly = isDesktop(),
   playTime = 5000;
 
 $topSlider.royalSlider({
-  addActiveClass: sliderWatch,
+  addActiveClass: desktopOnly,
   controlNavigation: "none",
   imageScaleMode: "fill",
   slidesSpacing: 0,
@@ -23,13 +23,14 @@ $topSlider.royalSlider({
 });
 
 $workCarousel.royalSlider({
-  addActiveClass: sliderWatch,
+  addActiveClass: desktopOnly,
   controlNavigation: "none",
   imageScaleMode: "fill",
   slidesSpacing: 0,
   numImagesToPreload: 1,
   
   keyboardNavEnabled: true,
+  navigateByClick: desktopOnly,
   visibleNearby: {
     enabled: true,
     center: false,
@@ -55,6 +56,8 @@ if ($workCarouselConfig) {
   $workCarouselConfig.ev.on("rsBeforeAnimStart", function(event) {
     var
       fromEnd = event.target._prevSlideId == event.target.numSlides - 1
+
+    $portfolio.find(".hover--is-active").removeClass("hover--is-active");
 
     if (fromEnd) {
       $portfolio.removeClass("carousel--at-end");
