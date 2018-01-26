3771d934ad6ff9cc84d71879819f068d6075a5aa
Christian Jánský <chrisjansky@gmail.com>
2018-01-26 10:24:57 +0100

    KSS Deleted styleguide

--- app/assets/kss/public/guide.js
+++
@@ -1,8 +0,0 @@
-var
-  ref = $("[data-menu]").data("kss-ref"),
-  $subMenu = $("[data-submenu]"),
-  $menuItem = $("[data-menu-item]")
-
-if ($subMenu.length) {
-  $subMenu.appendTo($menuItem.eq(ref - 1));
-}
