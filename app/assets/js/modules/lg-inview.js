ea731a467be0a3a039928ac70a2898fe959f737c
Christian Jánský <chrisjansky@gmail.com>
2018-11-13 20:30:50 +0100

    More motion design improvements

--- app/assets/js/modules/lg-inview.js
+++ app/assets/js/modules/lg-inview.js
@@ -1,9 +1,19 @@
 module.exports = {
   init: (function instance() {
+    inView.threshold(0.25);
+
     inView('[data-inview]').on('enter', function(element) {
       element.classList.add('is--in-view');
     });
-  
+
+    inView('[data-present="container"]')
+    .on('enter', function(element) {
+      element.classList.add('is--present');
+    })
+    .on('exit', function(element) {
+      element.classList.remove('is--present');
+    });
+
     /* Return to make later calls possible */
     return instance;
   /* Auto run */
